237f3970f19e112fe970d04c93b6bbdcdeeebea0
BezymiannyiOlexii <71652686+BezymiannyiOlexii@users.noreply.github.com>
2020-12-27 02:17:11 +0200

    Add files via upload

---
+++ help_2/GetData.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using RestSharp;
+
+namespace Web_api
+{
+    [TestFixture()]
+    class Task_2: Initilize
+    {
+        [Test()]
+        public void GetMetaData()
+        {
+            RestClient restClient = new RestClient("https://api.dropboxapi.com/2/files/get_metadata");
+
+            RestRequest restRequest = Initilize.Create_New_and_Format();
+            Initilize.AddHeader_AddJsonBody();
+
+            var response = restClient.Execute(restRequest);
+            Assert.IsTrue(response.Content != null);
+        }
+    }
+}
